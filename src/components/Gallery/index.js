96302dea8beebba15d9829f4b9b936a1420b0b51
DeathByDusk <elijahkanellakis@gmail.com>
2022-05-16 15:52:03 -0500

    added hardcoded example to Gallery component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import photo from "../../assets/small/webApps/fullFill.png"
 
-function Gallery() {
+function Gallery(props) {
+  const currentCategory = {
+    name: "webApps",
+    description:
+      "Deployed full stack web applications, each using MVC development structure and back end databases."
+  };
+
   return (
     <section>
-      <h1>NAME</h1>
-      <p>DESCRIPTION</p>
+      <h1>{currentCategory.name}</h1>
+      <p>{currentCategory.description}</p>
+      <div className="flex-row">
+        <img
+          src={photo}
+          alt="Web App Example"
+          className="img-thumbnail mx-1"
+        />
+      </div>
     </section>
   );
 }
