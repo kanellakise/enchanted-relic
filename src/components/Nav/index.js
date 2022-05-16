4328d44bb4dd3704e98e89dec35f387cba58669f
DeathByDusk <elijahkanellakis@gmail.com>
2022-05-16 14:48:10 -0500

    added categories to nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
 function Nav() {
-const categories = [
-  {
-    name: "webApps",
-    title: "Web Apps",
-    description: "Deployed full stack web applications, each usind MVC development structure and back end databases."
-  },
-  {
-    name: "basicApps",
-    title: "Basic Apps",
-    description: "These applications were created using HTML5, CSS3, some JavaScript, and are deployed directly to GitHub Pages."
-  },
-  {
-    name: "commandLineApps",
-    title: "Command Line Apps",
-    description: "These applications require to be run through the command line with help of Node.js."
-  }
-]
+  const categories = [
+    {
+      name: "webApps",
+      title: "Web Apps",
+      description: "Deployed full stack web applications, each usind MVC development structure and back end databases."
+    },
+    {
+      name: "basicApps",
+      title: "Basic Apps",
+      description: "These applications were created using HTML5, CSS3, some JavaScript, and are deployed directly to GitHub Pages."
+    },
+    {
+      name: "commandLineApps",
+      title: "Command Line Apps",
+      description: "These applications require to be run through the command line with help of Node.js."
+    }
+  ]
 
+  function categorySelected(name) {
+    console.log(`${name} clicked`)
+  }
 
   return (
     <header>
@@ -34,22 +37,22 @@ const categories = [
               About Me
             </a>
           </li>
-            {categories.map((category) => (
-              <li
-                className="mx-1"
-                key={category.name}
-                >
-                  <span>
-                    {category.title}
-                  </span>
-                </li>
-            ))}
           <li className="mx-2">
             <span>Contact</span>
           </li>
           <li className="mx-2">
             <a href="https://drive.google.com/file/d/1rmrTVq4HOX8tmwCUQY8sUeiLhjWYpm3U/view?usp=sharing" target="about_blank">Resume</a>
           </li>
+          {categories.map((category) => (
+            <li
+              className="mx-1"
+              key={category.name}
+            >
+              <span onClick={() => categorySelected(category.name)}>
+                {category.title}
+              </span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
